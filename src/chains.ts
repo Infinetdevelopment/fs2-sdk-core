af8660c11ccad667ddd3df45a27331cd0b10fcb8
Ryan Hungate <ryanhungate@icloud.com>
2024-03-11 09:02:42 -0400

    build with less chains

--- src/chains.ts
+++ src/chains.ts
@@ -1,17 +1,15 @@
-export declare enum ChainId {
-       MAINNET = 369,
-       TESTNET = 943,
-       ETHEREUM = 1,
-       GOERLI = 5,
+export enum ChainId {
+  MAINNET = 369,
+  TESTNET = 943,
+  ETHEREUM = 1,
+  GOERLI = 5,
 }
-export declare const SUPPORTED_CHAINS: readonly [
-  ChainId.MAINNET,
-  ChainId.TESTNET,
-  ChainId.ETHEREUM,
-  ChainId.GOERLI
-];
-export declare type SupportedChainsType = typeof SUPPORTED_CHAINS[number];
-export declare enum NativeCurrencyName {
-    PULSECHAIN = "PLS",
-    ETHER = "ETH"
+
+export const SUPPORTED_CHAINS = [ChainId.MAINNET, ChainId.TESTNET, ChainId.ETHEREUM, ChainId.GOERLI,]
+
+export type SupportedChainsType = typeof SUPPORTED_CHAINS[number]
+
+export enum NativeCurrencyName {
+  PULSECHAIN = 'PLS',
+  ETHER = 'ETH'
 }
